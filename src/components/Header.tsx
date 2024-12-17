8e267c720065936882a33812711cc3656aa96b86
hebonilla <herbert.david37@gmail.com>
2024-12-16 18:24:44 -0600

    Header component development

---
+++ src/components/Header.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useCallback } from 'react';
+import { AppBar, Toolbar, InputAdornment, Box, Container, Grid, IconButton, TextField, Typography } from '@mui/material';
+import { debounce } from 'lodash';
+import SearchIcon from '@mui/icons-material/Search';
+
+interface HeaderProps {
+  onSearch: (query: string) => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const debouncedSearch = useCallback(
+    debounce((query: string) => {
+      onSearch(query);
+    }, 300),
+    [onSearch]
+  );
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+    debouncedSearch(query);
+  };
+
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        lineHeight: '0rem',
+        marginTop: '-4rem'
+      }}
+    >
+      <Box
+        component="img"
+        sx={{
+          height: { xs: 373, md: 400 },
+          width: '100%',
+          maxHeight: { xs: 373, md: 400 },
+          maxWidth: '100%',
+          objectFit: 'cover',
+        }}
+        src='https://d296xu67oj0g2g.cloudfront.net/lm_cms/images/CMS/OVERVIEW%20BANNERS/1024/CLUB_HB1.png'
+      />
+      {/* Overlay layer */}
+      <div className={'headerContainer'}>
+        <Container maxWidth="lg">
+          <Grid
+            container
+            sx={{
+              paddingBottom: '2rem',
+            }}
+          >
+            <Grid item xs={12}>
+              <Typography 
+                variant="h2"
+                color={'text.primary'}
+                gutterBottom
+                sx={{
+                  fontSize: {
+                    xs: '2rem',
+                    sm: '4rem',
+                  },
+                }}
+              >
+                Weather App
+              </Typography>
+            </Grid>
+            {/* Form */}
+            <Grid item xs={12}>
+              <br></br>
+              <TextField
+                sx={{
+                  backgroundColor: 'rgb(0,0,0,0.6)',
+                }}
+                fullWidth
+                id="filled-multiline-static"
+                label="Type a city"
+                size='small'
+                type='text'
+                variant='filled'
+                color='secondary'
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    </Box>
+  );
+};
+
+export default Header;
