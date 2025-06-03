fb5828ad78463c59c1ded60e34379f5b524297a5
hebonilla <herbert.ayala@lifemiles.com>
2025-06-03 14:32:30 -0600

    Popup true

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -59,7 +59,7 @@ export default function Menu() {
   }, []);
 
   const handleLogin = () => {
-    window.lmLogin?.(false, {
+    window.lmLogin?.(true, {
       onSuccess: async () => {
         await getBalance();
       },
