a4aa3887817ab329f13d7fbae6a05c3b0dba286a
Herbert <herbert.ayala@lifemiles.com>
2025-07-22 09:58:52 -0600

    Script env UAT test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import Head from 'next/head';
-import { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import theme from '../styles/theme';
-import '../styles/styles.css';
-import createEmotionCache from '../utils/createEmotionCache';
+import React, { useEffect } from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../styles/theme";
+import "../styles/styles.css";
+import createEmotionCache from "../utils/createEmotionCache";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -19,17 +19,17 @@ export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   useEffect(() => {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     //script.src = 'https://s3-eu-west-1.amazonaws.com/lifemileswebsite-s3bucket-41/lm_cms/images/EON/LOGIN/lm-login.umd.js?env=uat';
-    script.src = '/lm-login-popup.umd.js?env=qa';
+    script.src = "/lm-login-popup.umd.js?env=uat";
     script.async = true;
 
     script.onload = () => {
-      console.log('Script cargado correctamente');
+      console.log("Script cargado correctamente");
     };
 
     script.onerror = () => {
-      console.error('Error al cargar el script');
+      console.error("Error al cargar el script");
     };
 
     document.body.appendChild(script);
@@ -42,7 +42,7 @@ export default function MyApp(props: MyAppProps) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
